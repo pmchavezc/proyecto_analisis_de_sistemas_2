c6bd1b9bba996cd5bb94659e2fa1620b23c4ee36
Jonathan Guamuch <joelmorales0598@gmail.com>
2025-10-30 09:39:34 -0600

    Redireccionando dockerfile

--- MantenimientoUrbano-Backend/src/main/java/proyecto/MantenimientoUrbano_Backend/infrastructure/config/WebController.java
+++ MantenimientoUrbano-Backend/src/main/java/proyecto/MantenimientoUrbano_Backend/infrastructure/config/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/{path:^(?!api|static|swagger-ui).*}/**")
+    @GetMapping(value = {"/{path:[^\\.]*}", "/"})
     public String redirect() {
         return "forward:/index.html";
     }
